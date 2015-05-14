77989c01c75a4af4d3e4f6cf1bf19e049984bb05
nsahoo <niladribihari.sahoo@cern.ch>
2015-05-15 01:31:04 +0200

    updated the config file

--- CMSSW_7_4_0/src/BphTRG/HLTEffAnalyzer/test/testHLT_cfg.py
+++ CMSSW_7_4_0/src/BphTRG/HLTEffAnalyzer/test/testHLT_cfg.py
@@ -17,7 +17,8 @@ process.source = cms.Source("PoolSource",
                             skipEvents = cms.untracked.uint32( 0 ),
 
                             fileNames = cms.untracked.vstring( 
-                            '/store/relval/CMSSW_7_4_0_pre5/RelValBuJpsiK_13/GEN-SIM-RECO/MCRUN2_73_V7-v1/00000/F0B65CB2-F79D-E411-8A04-002618943842.root'
+#                            '/store/relval/CMSSW_7_4_0_pre5/RelValBuJpsiK_13/GEN-SIM-RECO/MCRUN2_73_V7-v1/00000/F0B65CB2-F79D-E411-8A04-002618943842.root'
+                            '/store/mc/Phys14DR/TT_Tune4C_13TeV-pythia8-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/007B37D4-8B70-E411-BC2D-0025905A6066.root'
                             )
 )
 
